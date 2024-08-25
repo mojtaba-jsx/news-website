6f7c1aa337584f33c834b85da9aeed090cd97999
mojtaba-jsx <mojtaba.jsx@gmail.com>
2024-08-25 12:09:05 +0330

    [Header]- Add CSS Styles For Navbar And Add Submenu For Categories

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Header.css";
+import "../../custom.css";
 import { CiSearch } from "react-icons/ci";
 import { IoIosArrowDown } from "react-icons/io";
 import { CiBookmark } from "react-icons/ci";
+import { FcMms } from "react-icons/fc";
+import { FcBullish } from "react-icons/fc";
+import { FcPhoneAndroid } from "react-icons/fc";
+import { FaBasketballBall } from "react-icons/fa";
+import { FcGraduationCap } from "react-icons/fc";
+import { FcCollaboration } from "react-icons/fc";
 function Header() {
   return (
     <>
@@ -10,19 +17,61 @@ function Header() {
         <nav className="navbar">
           {/* //! Navbar Logo */}
           <div className="navbar__logo">
-            <img
+            {/* <img
               src="./src\assets\images\logo.jpeg"
               alt="Logo"
               className="navbar__logo-img"
-            />
+            /> */}
+            مگا نیوز
           </div>
 
           {/* //! Navbar Menu */}
           <ul className="navbar__menu">
-            <li className="navbar__menu-item">
-              <a href="#" className="navbar__menu-link">
+            <li className="navbar__menu-item submenu-hover">
+              <a href="#" className="navbar__menu-link ">
                 دسته بندی ها
+                <IoIosArrowDown className="navbar__menu-link-icon" />
               </a>
+              <div className="submenu">
+                <ul className="submenu__list">
+                  <li className="submenu__item">
+                    <a href="#" className="submenu__link">
+                      سرگرمی
+                      <FcMms className="submneu__icon" />
+                    </a>
+                  </li>
+                  <li className="submenu__item">
+                    <a href="#" className="submenu__link">
+                      اقتصادی
+                      <FcBullish className="submneu__icon" />
+                    </a>
+                  </li>
+                  <li className="submenu__item">
+                    <a href="#" className="submenu__link">
+                      تکنولوژی
+                      <FcPhoneAndroid />
+                    </a>
+                  </li>
+                  <li className="submenu__item">
+                    <a href="#" className="submenu__link">
+                      ورزشی
+                      <FaBasketballBall className="submneu__icon" />
+                    </a>
+                  </li>
+                  <li className="submenu__item">
+                    <a href="#" className="submenu__link">
+                      علمی
+                      <FcGraduationCap className="submneu__icon" />
+                    </a>
+                  </li>
+                  <li className="submenu__item">
+                    <a href="#" className="submenu__link">
+                      اجتماعی
+                      <FcCollaboration className="submneu__icon" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li className="navbar__menu-item">
               <a href="#" className="navbar__menu-link">
@@ -55,13 +104,13 @@ function Header() {
                 className="navbar__info__admin-image"
               />
               <span className="navbar__info__admin__name">
-                Mojtaba
+                مجتبی
                 <IoIosArrowDown className="navbar__info__admin__name-icon" />
               </span>
             </div>
 
             <div className="navbar__info__saved">
-            <CiBookmark className="navbar__info__saved-icon" />
+              <CiBookmark className="navbar__info__saved-icon" />
             </div>
           </div>
         </nav>
