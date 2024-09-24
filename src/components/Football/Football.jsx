b437e0bdb1923c635a055fb6df7c55e4e544cf5a
mojtaba-jsx <mojtaba.jsx@gmail.com>
2024-09-24 08:58:54 +0330

    [Football]- Fix Bugs And Add Responsive

--- src/components/Football/Football.jsx
+++ src/components/Football/Football.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Football.css";
-import FootballCalender from "../FootballCalender/FootballCalender";
+// import FootballCalender from "../FootballCalender/FootballCalender";
 import FootballTime from "../FootballTime/FootballTime";
 import FootballTable from "../FootballTable/FootballTable";
 function Football() {
   return (
     <div className="football">
-      <FootballCalender />
+      {/* <FootballCalender /> */}
       <FootballTable />
       <FootballTime />
     </div>
